07e98597aa15d01fbfa67a500e7582a5e12d73a8
Fredrik <fl@kommandokringla.se>
2022-10-04 19:13:23 +0200

    Lösning ett, ganska simpel

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-// Skriv din JS här
+// Spara alla h2 i en array
+const headings = document.getElementsByClassName('accordion');
+
+// Loopa över listan med h2
+for (i=0; i<headings.length; i++) {
+  // För varje h2, lägg till en eventlistener
+  headings[i].addEventListener('click', function () {
+    // Vid ett click event, toggla klassen text
+    this.nextElementSibling.classList.toggle('text');
+  })
+}
